c518c8cad1f2e2c02c1d735ca5c518ad4f20c474
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-04-07 18:21:34 +0200

    Add Reddit Service to fetch RSS feed

---
+++ src/services/reddit.service.ts
@@ -0,0 +1,20 @@
+import React from "react";
+import { XMLParser, XMLBuilder, XMLValidator } from "fast-xml-parser";
+
+class RedditService {
+  private readonly BASE_URL =
+    "https://mighty-earth-63459.herokuapp.com/https://www.reddit.com/.rss";
+
+  parser = new XMLParser();
+
+  public async getFrontPage() {
+    fetch(this.BASE_URL)
+      .then((response) => response.text())
+      .then((str) => {
+        let jObj = this.parser.parse(str);
+        console.log(jObj.feed.entry);
+      });
+  }
+}
+
+export const redditService = new RedditService();
