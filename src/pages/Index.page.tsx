218c03aa44a677523687abc282283e86645dc620
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-04-28 20:40:21 +0200

    Add Footer component

--- src/pages/Index.page.tsx
+++ src/pages/Index.page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import PostContainer from "../components/PostContainer";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Index() {
   return (
     <>
       <Header></Header>
       <PostContainer></PostContainer>
+      <Footer></Footer>
     </>
   );
 }
