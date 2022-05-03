3bc9729c21bf65948b686d024e3dfaecd185deea
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-05-03 14:04:29 +0200

    Add react routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import logo from "./logo.svg";
+import Detail from "./pages/Detail.page";
 import Index from "./pages/Index.page";
 import "./scss/global.scss";
 
 function App() {
   return (
     <>
-      <Index></Index>
+      <Header></Header>
+      <Routes>
+        <Route path="post">
+          <Route path=":id" element={<Detail />} />
+        </Route>
+        <Route path="/" element={<Index />} />
+      </Routes>
+      <Footer></Footer>
     </>
   );
 }
---
+++ src/pages/Detail.page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export default function Detail() {
+  const params = useParams();
+  return <>{params.id?.toString()}</>;
+}
