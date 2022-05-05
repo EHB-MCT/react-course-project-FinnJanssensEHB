58fb36d3c2ba832dcc3b1b14acd405125ae20888
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-05-05 16:53:45 +0200

    Fix CreateRoot issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./scss/index.scss";
 import App from "./App";
 import { Provider } from "react-redux";
@@ -7,15 +8,16 @@ import * as serviceWorker from "./serviceWorker";
 import { store } from "./store";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
